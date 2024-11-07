42338bd583d804df2f17b61a1faf22054485f582
codeeon <yeon.develop@gmail.com>
2024-11-08 02:31:16 +0900

    Feat: add utils

--- src/__tests__/utils.ts
+++ src/__tests__/utils.ts
@@ -1,3 +1,4 @@
+import { Event } from '../types';
 import { fillZero } from '../utils/dateUtils';
 
 export const assertDate = (date1: Date, date2: Date) => {
@@ -9,4 +10,20 @@ export const parseHM = (timestamp: number) => {
   const h = fillZero(date.getHours());
   const m = fillZero(date.getMinutes());
   return `${h}:${m}`;
+};
+
+interface HasIdType {
+  id: string;
+}
+
+export const createNewId = <T extends HasIdType>(list: T[]) => {
+  if (list.length === 0) {
+    return '1';
+  }
+
+  return (Number(list[list.length - 1].id) + 1).toString();
+};
+
+export const sortEventList = (events: Event[]) => {
+  return events.sort((a, b) => Number(a.id) - Number(b.id));
 };
