8c7e79e4fcc1831a0ebd13b08d82662b42cf1161
codeeon <yeon.develop@gmail.com>
2024-11-08 02:29:47 +0900

    Feat: handlers

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -2,15 +2,57 @@ import { http, HttpResponse } from 'msw';
 
 import { Event } from '../types';
 import { events } from './response/events.json' assert { type: 'json' };
+import { createNewId } from '../__tests__/utils';
+
+let eventList: Event[] = [...(events as Event[])];
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json({ events: eventList });
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const event = (await request.json()) as Event | Omit<Event, 'id'>;
 
-  http.put('/api/events/:id', async () => {}),
+    const newId = createNewId(eventList as Event[]);
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+    const newEvent = {
+      ...event,
+      id: newId,
+    };
+
+    eventList = [...eventList, newEvent];
+
+    return HttpResponse.json(
+      eventList.find((event) => event.id === newEvent.id),
+      { status: 201 }
+    );
+  }),
+
+  http.put('/api/events/:id', async ({ params, request }) => {
+    const { id } = params;
+
+    const updates = (await request.json()) as Event;
+
+    if (!id || !eventList.find((event) => event.id === id)) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    eventList = eventList.map((event) => (event.id === id ? { ...event, ...updates } : event));
+
+    return HttpResponse.json(
+      eventList.find((event) => event.id === id),
+      { status: 200 }
+    );
+  }),
+
+  http.delete('/api/events/:id', ({ params }) => {
+    const { id } = params;
+
+    eventList = eventList.filter((event) => event.id !== id);
+
+    return new HttpResponse(null, { status: 204 });
+  }),
 ];
